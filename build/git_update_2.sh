a5b9fa2722a80a28dec1bccec7156a4e08197888
tannv-devv <tannvde31@gmail.com>
2023-07-23 22:41:59 +0700

    fix tag

--- build/git_update_2.sh
+++ build/git_update_2.sh
@@ -3,6 +3,9 @@
 VERSION=""
 ENVIRONMENT=${GITHUB_REF##*/}
 
+git config --global user.email "github-actions[bot]@users.noreply.github.com"
+git config --global user.name "github-actions[bot]"
+
 # get parameters
 while getopts v: flag
 do
