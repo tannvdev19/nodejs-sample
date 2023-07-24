fd496c8a80a2bc69b5f272d271e4532885bd047f
tannv-devv <tannvde31@gmail.com>
2023-07-24 14:44:34 +0700

    fix tag

--- build/create_new_tag.sh
+++ build/create_new_tag.sh
@@ -8,7 +8,7 @@ git config --global user.email "github-actions[bot]@users.noreply.github.com"
 git config --global user.name "github-actions[bot]"
 
 # get parameters
-while getopts v:e: flag
+while getopts e:v: flag
 do
   case "${flag}" in
     v) VERSION=${OPTARG};;
