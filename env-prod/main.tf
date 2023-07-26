aedccf0d26295aeab2439638f1ecb02669548212
tannv-devv <tannvde31@gmail.com>
2023-07-26 08:59:48 +0700

    fix bug

--- env-prod/main.tf
+++ env-prod/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-state-tannv"
-    key    = "state-dev/terraform.tfstate"
+    key    = "state-prod/terraform.tfstate"
     region = "ap-southeast-1"
   }
 }
