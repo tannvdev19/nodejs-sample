f41cda55f8886b76c5a76cb06a26f029dd98f8c9
tannv-devv <tannvde31@gmail.com>
2023-07-26 08:28:50 +0700

    fix bug

--- bash-script/update-vars.sh
+++ bash-script/update-vars.sh
@@ -2,7 +2,10 @@
 
 # Update the variables in the github environment
 
-VARIABLE_ENVIRONMENT_NAME="ECR_REPO_URL"
+GH_VARIABLE_ENVIRONMENT_NAME="ECR_REPO_URL"
+GH_ECR_REGISTRY_NAME = "ECR_REGISTRY"
+GH_ECR_REPOSITORY_NAME = "ECR_REPOSITORY"
+
 ECR_REPO_URL=""
 ENVIRONMENT=""
 
@@ -21,5 +24,26 @@ echo "ENVIRONMENT: $ENVIRONMENT"
 # update the environment variables
 gh variable set $VARIABLE_ENVIRONMENT_NAME -b $ECR_REPO_URL -e $ENVIRONMENT
 
+
 echo "gh variable set $VARIABLE_ENVIRONMENT_NAME -b $ECR_REPO_URL -e $ENVIRONMENT"
+
+
+
+string=ECR_REPO_URL
+
+# Save the current IFS value to restore it later
+oldIFS=$IFS
+# Set the IFS to "/"
+IFS="/"
+
+# Split the string into an array using the IFS
+read -ra parts <<< "$string"
+
+# Restore the IFS to its original value
+IFS=$oldIFS
+
+# Loop through the array to access each part
+for part in "${parts[@]}"; do
+    echo "$part"
+done
 
