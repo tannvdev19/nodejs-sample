863e55d0ea49902056304fc976f95e350109ca2c
tannv-devv <tannvde31@gmail.com>
2023-07-26 08:47:09 +0700

    fix bug

--- bash-script/update-vars.sh
+++ bash-script/update-vars.sh
@@ -3,8 +3,8 @@
 # Update the variables in the github environment
 
 GH_VARIABLE_ENVIRONMENT_NAME="ECR_REPO_URL"
-GH_ECR_REGISTRY_NAME = "ECR_REGISTRY"
-GH_ECR_REPOSITORY_NAME = "ECR_REPOSITORY"
+GH_ECR_REGISTRY_NAME="ECR_REGISTRY"
+GH_ECR_REPOSITORY_NAME="ECR_REPOSITORY"
 
 ECR_REPO_URL=""
 ENVIRONMENT=""
@@ -23,10 +23,6 @@ echo "ENVIRONMENT: $ENVIRONMENT"
 
 # update the environment variables
 gh variable set $VARIABLE_ENVIRONMENT_NAME -b $ECR_REPO_URL -e $ENVIRONMENT
-
-
-echo "gh variable set $VARIABLE_ENVIRONMENT_NAME -b $ECR_REPO_URL -e $ENVIRONMENT"
-
 
 
 # Save the current IFS value to restore it later
