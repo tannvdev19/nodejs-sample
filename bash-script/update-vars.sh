e10838e01f3286f8f1536a157851b5b17e556eab
tannv-devv <tannvde31@gmail.com>
2023-07-26 08:59:03 +0700

    fix bug

--- bash-script/update-vars.sh
+++ bash-script/update-vars.sh
@@ -21,10 +21,6 @@ done
 echo "ECR_REPO_URL: $ECR_REPO_URL"
 echo "ENVIRONMENT: $ENVIRONMENT"
 
-# update the environment variables
-gh variable set $VARIABLE_ENVIRONMENT_NAME -b $ECR_REPO_URL -e $ENVIRONMENT
-
-
 # Save the current IFS value to restore it later
 oldIFS=$IFS
 # Set the IFS to "/"
@@ -36,12 +32,8 @@ read -ra parts <<< "$ECR_REPO_URL"
 # Restore the IFS to its original value
 IFS=$oldIFS
 
-# Loop through the array to access each part
-echo ${#parts[@]}
-for part in "${parts[@]}"; do
-    echo "$part"
-done
-
+# update the environment variables
+gh variable set $VARIABLE_ENVIRONMENT_NAME -b $ECR_REPO_URL -e $ENVIRONMENT
 gh variable set $GH_ECR_REGISTRY_NAME -b ${parts[0]} -e $ENVIRONMENT
 gh variable set $GH_ECR_REPOSITORY_NAME -b ${parts[1]} -e $ENVIRONMENT
 
