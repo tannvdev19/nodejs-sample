0132524fbd88bb7ff53fe9c8ca71a2b414212f67
tannv-devv <tannvde31@gmail.com>
2023-07-27 15:05:03 +0700

    fix bug

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -2,7 +2,7 @@ resource "aws_ecr_repository" "ecr_repo" {
   name = var.ecr_name
 
   image_scanning_configuration {
-    scan_on_push = true
+    scan_on_push = false
   }
 
   encryption_configuration {
