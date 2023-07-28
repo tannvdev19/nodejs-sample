d17f521b156faf2e5ad3dccdeacd0577ba081e24
tannv-devv <tannvde31@gmail.com>
2023-07-28 14:35:36 +0700

    fix bug

--- env-prod/outputs.tf
+++ env-prod/outputs.tf
@@ -5,11 +5,11 @@ output "ecr_repo_url" {
 
 
 output "ec2_public_ip" {
-  value       = module.dev_ec2.ec2_public_ip
+  value       = module.prod_ec2.ec2_public_ip
   description = "The EC2 public IP"
 }
 
 output "ec2_public_dns" {
-  value       = module.dev_ec2.ec2_public_dns
+  value       = module.prod_ec2.ec2_public_dns
   description = "The EC2 public DNS"
 }
